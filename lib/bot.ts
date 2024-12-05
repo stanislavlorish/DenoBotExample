e870d589d93a6ab9b37bc6250a5a8fcd2f2a0aa0
stanislavlorish <121788378+stanislavlorish@users.noreply.github.com>
2024-12-05 17:05:55 +0600

    Update bot.ts

--- lib/bot.ts
+++ lib/bot.ts
@@ -1,33 +1,33 @@
 import { Bot, InlineKeyboard } from "https://deno.land/x/grammy@v1.32.0/mod.ts";
 
 export const bot = new Bot(Deno.env.get("BOT_TOKEN") || "");
-const userInterests = new Map<string, string>(); // Храним интересы пользователей
+const userInterests = new Map<string, string>(); 
 
-// Обработайте команду /start.
+
 bot.command("start", (ctx) => 
     ctx.reply("Добро пожаловать. Запущен и работает!", { reply_markup: keyboard })
 );
 
-// Обработайте команду /interested.
+
 bot.command("interested", (ctx) => {
     ctx.reply("Напишите свой интерес, и я его запомню!");
 });
 
-// Обработка сообщений от пользователей для сохранения интересов
+
 bot.on("message", (ctx) => {
     const userId = ctx.from?.id.toString();
     
     if (userId) {
-        // Проверяем, хранится ли интерес пользователя
+      
         if (userInterests.has(userId) && userInterests.get(userId) === '') {
             const interest = ctx.message.text;
             userInterests.set(userId, interest);
             ctx.reply(`Ваш интерес "${interest}" сохранен!`);
         }
-        // Если пользователь еще не отправил интерес
+      
         else if (!userInterests.has(userId)) {
             ctx.reply("Пожалуйста, используйте команду /interested, чтобы начать.");
-            userInterests.set(userId, ''); // Запоминаем, что пользователь пожелал отправить интерес
+            userInterests.set(userId, ''); 
         }
     } else {
         ctx.reply("Не могу определить ваш ID.");
@@ -44,7 +44,7 @@ bot.callbackQuery("/about", async (ctx) => {
 });
 
 // Запускаем бота
-await bot.start(); // Используем await для ожидания завершения запуска
+ bot.start(); 
 
 
 
