deff96ff4b0c6b9c66ddce7a2841c3b201707b96
stanislavlorish <121788378+stanislavlorish@users.noreply.github.com>
2024-12-05 17:17:33 +0600

    Update bot.ts

--- lib/bot.ts
+++ lib/bot.ts
@@ -1,18 +1,17 @@
 import { Bot, InlineKeyboard } from "https://deno.land/x/grammy@v1.32.0/mod.ts";
 
-export const bot = new Bot(Deno.env.get("BOT_TOKEN") || "");
+export const bot = new Bot(Deno.env.get("BOT_TOKEN") || ""); // Токен вашего бота
 
 const keyboard = new InlineKeyboard()
     .text("Обо мне", "/about")
     .text("Интересы", "/interests");
 
+// Обработайте команду /start.
 bot.command("start", (ctx) => ctx.reply("Добро пожаловать. Запущен и работает!", { reply_markup: keyboard }));
 
-bot.on("message", (ctx) => ctx.reply("Получил ваше сообщение: " + ctx.message.text + " !"));
-
-// Обработка команды /about
+// Обработайте команду /about
 bot.callbackQuery("/about", async (ctx) => {
-    await ctx.answerCallbackQuery();
+    await ctx.answerCallbackQuery(); // Уведомляем Telegram, что мы обработали запрос
     await ctx.reply("Я бот? Я бот... Я Бот!");
 });
 
@@ -21,17 +20,15 @@ bot.command("interests", async (ctx) => {
     await ctx.reply("Напиши свой интерес:");
 });
 
-// Обработка текстовых сообщений после команды /interests
+// Обработка сообщений, полученных после команды /interests
 bot.on("message", (ctx) => {
-    if (ctx.message.text && ctx.message.text.startsWith("Интерес: ")) {
-        // Это сообщение уже обрабатывается для других случаев. Пропускаем его.
-        return;
+    if (ctx.message.text && ctx.message.text !== "/interests" && !ctx.message.text.startsWith("Ваш интерес: ")) {
+        ctx.reply("Ваш интерес: " + ctx.message.text);
     }
-    
-    ctx.reply("Ваш интерес: " + ctx.message.text);
 });
-bot.start();
 
+// Обработайте другие сообщения.
+bot.on("message", (ctx) => ctx.reply("Получил ваше сообщение: " + ctx.message.text + " !"));
 
 
 
